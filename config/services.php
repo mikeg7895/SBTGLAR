be9a1c1fcba600f0cf91a5f3ba91e8213d25bf2b
maykoll <maykollgomez050@gmail.com>
2023-12-21 09:30:56 -0500

    Nelson lo arreglo

--- config/services.php
+++ config/services.php
@@ -34,6 +34,6 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => 'http://127.0.0.1:8000/google-auth/callback',
+        'redirect' => 'https://sbtglar-production.up.railway.app/google-auth/callback',
     ],
 ];
